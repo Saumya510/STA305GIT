53aa8899776c37c061958075b30ffff442d7c58e
Saumya Bakshi <bakshi.saumya10@gmail.com>
2022-02-09 15:30:43 -0500

    Final Files

--- Paper1/scripts/01-data_cleaning.R
+++ Paper1/scripts/01-data_cleaning.R
@@ -1,8 +1,8 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Test the Data Cleaning and Analysis for COVID-19 Toronto Data
+# Author: Saumya Bakshi
+# Data: 9 February 2021
+# Contact: saumya.bakshi@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 
 # - Need to have downloaded the ACS data and saved it to inputs/data
@@ -13,24 +13,64 @@
 
 #### Workspace setup ####
 # Use R Projects, not setwd().
-library(haven)
-library(tidyverse)
+library(opendatatoronto)
+library(dplyr)
+library(janitor)
+library(ggplot2)
+library(knitr)
+library(kableExtra)
+library(gridExtra)
+library(grid)
 # Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
+neighbourhoods <- 
+  list_package_resources("https://open.toronto.ca/dataset/neighbourhoods/") %>% get_resource()
+covid_dataset <- 
+  list_package_resources("https://open.toronto.ca/dataset/covid-19-cases-in-toronto/") %>% 
+  get_resource()
+
 # Just keep some variables that may be of interest (change 
 # this depending on your interests)
-names(raw_data)
+covid_mini = subset(covid_dataset, select=c("_id", "Age Group", "Classification", "Outcome"))
+covid_mini <- clean_names(covid_mini)
+covid_mini <- na.omit(covid_mini) 
+covid_mini <- filter(covid_mini, covid_mini$classification == "CONFIRMED")
 
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
+head_ = head(covid_mini)
+
+kable(head_, caption = "COVID-19 Data Subset", booktabs = T) %>%
+  kable_styling(full_width = T)
          
 
 #### What's next? ####
 
+
+### Pivot Table Creation ###
+
+pivot_age_group = covid_mini %>%
+  select(age_group, id, outcome) %>%
+  group_by(age_group) %>%
+  summarize(total_cases = length(!is.na(id)), total_deaths = length(outcome[outcome=="FATAL"]), 
+            mortality_rate = total_deaths/total_cases)
+
+kable(pivot_age_group, caption = "COVID 19 Cases by Age Group", booktabs = T) %>%
+  kable_styling(full_width = T)
+
+### Plot figures ###
+
+plot1 <- 
+  ggplot(pivot_age_group) +
+  aes(x = age_group, y = total_cases) +
+  geom_point(colour = "#0c4c8a") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+plot2 <- 
+  ggplot(pivot_age_group) +
+  aes(x = age_group, y = total_deaths) +
+  geom_point(colour = "#FF0000") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+grid.arrange(plot1, plot2, ncol=2)
 
 
          
